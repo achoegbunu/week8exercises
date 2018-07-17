10bf49506621b54b86e632aa151ea20b8e919cbb
acho <achoegbunu@gmail.com>
2018-07-16 21:58:53 -0700

    answered question 1-12 on week 8 exercises

--- js/main.js
+++ js/main.js
@@ -9,10 +9,32 @@ var myBody = document.getElementsByTagName("body")[0]
 myBody.appendChild(newDiv1)
 newDiv1.appendChild(newH1)
 
-var extraNewH1 = document.createElement("hi")
+var extraNewH1 = document.createElement("h1")
 extraNewH1.innerText = "umm, yeah it still works";
 var myBody = document.getElementsByTagName("body")[0]
 myBody.appendChild(newDiv1)
 newDiv1.appendChild(extraNewH1)
+
+var myDiv = document.getElementsByClassName("class1")[0]
+var newDiv2 = document.createElement("div2");
+var coolestNewH1 = document.createElement("h1");
+var newTextNode = document.createTextNode("my new h1 though, so cool");
+coolestNewH1.innerText = "my new h1 though, so cool";
+var myBody = document.getElementsByTagName("body")[0]
+myBody.appendChild(newDiv2)
+newDiv2.appendChild(coolestNewH1)
+
+	var image1 = document.createElement("IMG");
+	image1.id = "Arsenal";
+	image1.src = "https://wallpaper-house.com/data/out/9/wallpaper2you_311043.gif"
+	newDiv2.appendChild(image1)
+
+
+
+
+
+
+
+
 
 
